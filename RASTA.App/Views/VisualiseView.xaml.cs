249bc9c5a9bf0b225553824048c054b0c8c5cd13
JPhilC <phil@unitysoftware.co.uk>
2026-08-19 13:31:41 +0100

    Removed accidental button_click handler.

--- RASTA.App/Views/VisualiseView.xaml.cs
+++ RASTA.App/Views/VisualiseView.xaml.cs
@@ -24,10 +24,5 @@ namespace RASTA.App.Views
         {
             InitializeComponent();
         }
-
-        private void Button_Click(object sender, RoutedEventArgs e)
-        {
-
-        }
     }
 }
